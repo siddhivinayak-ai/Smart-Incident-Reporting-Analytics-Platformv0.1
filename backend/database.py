3a0b33877330ec2bb0d7f9f49deaf85fa866d401
siddhivinayak-ai <siddhivinayakwaghmode2004@gmail.com>
2026-01-30 20:15:57 +0530

    same db

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+
 CLIENT = AsyncIOMotorClient(MONGO_URL)
 DB = CLIENT["incident_db"]
 
